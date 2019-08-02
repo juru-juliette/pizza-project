6ec4900b9d0d10759c949b7b7c644cdf1143a050
juru-juliette <kangabejuliette@gmail.com>
2019-08-02 17:51:45 +0200

    adding prototype to the function

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ function Quest(size,crust,topping,flavour,number){
     this.chooseFlavour= flavour;
     this.chooseNumber = number;
 }
+Quest.prototype.fullOrder = function (){
+    return this.size + "," + this.crust + "," + this.toppings + "," + this.flavour + "," +this.number;
+}
 function resetfield(){
     $("input#checkbox").val();
     $("input#crust").val();
@@ -12,6 +15,18 @@ function resetfield(){
     $("input#toppings").val();
     $("input#number").val();
 }
+$(document).ready(function() {
+    $("form.form-order").submit(function(event) {
+        event.preventDefault();
+    
+        var inputtedSize= $("input#checkbox").val();
+        var inputtedCrust = $("input#crust").val();
+        var inputtedFlavour= $("input#flavour").val();
+        var inputtedTopping = $("input#toppings").val();
+        var inputtedNumber= $("input#number").val();
+        var newContact = new Contact(inputtedSize, inputtedCrust,inputtedFlavour,inputtedTopping,inputtedNumber);
+   
+    });
 
 
 
