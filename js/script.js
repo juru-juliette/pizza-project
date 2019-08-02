928b39cb4d6e45f86f72f1992396f681acd7fd5a
juru-juliette <kangabejuliette@gmail.com>
2019-08-02 15:52:50 +0200

    I created a function

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function Quest(size,crust,topping,flavour,number){
+    this.chooseSize = size;
+    this.chooseCrust = crust;
+    this.chooseToppings = toppings;
+    this.chooseFlavour= flavour;
+    this.chooseNumber = number;
+}
+function resetfield(){
+    $("input#checkbox").val();
+    $("input#crust").val();
+    $("input#flavour").val();
+    $("input#toppings").val();
+    $("input#number").val();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
