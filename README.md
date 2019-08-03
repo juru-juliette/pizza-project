6aabe4e49305690eb63f04de53777af9c2b7f1f5
juru-juliette <kangabejuliette@gmail.com>
2019-08-03 20:38:42 +0200

    adding read me

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pizza-project
 Order what you wants, we are ready to help you.
+# Pizza Quest
+## Description
+This is webpage for pizza quest shop, where you can get more delicious pizza on time also you can make order if you want 
+## Setup/Installation Requirements
+* visual studio code
+* To create a repository on github
+* bootstrap version 4.3.1
+## Technologies Used
+* html(hyper-text markup language)
+* css(cascading style sheet)
+* javascript
+* OOP(Object oriented programming)
+* library of js (jquery version 3.4.1)
+## Support and contact details
+* Contact: +250787873242 &+250789557608
+* Email: kangabejuliette@gmail.com
+### License
+Copyright (c) {2019} Pizza Quest
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function Quest(size, crust,flavour,toppings, number) {
     this.toppings = toppings;
     this.number = number;
 }
-Quest.prototype.orders = function () {
+Quest.prototype.fullOrder = function () {
     return this.size + "," + this.crust + "," + this.toppings + "," + this.flavour + "," + this.number;
 }
 
@@ -28,11 +28,11 @@ $(document).ready(function () {
     
     });
 
-    $("ul#call").append("<li><span class='pick'>" + order.orders() + "</span></li>");
+    $("ul#call").append("<li><span class='pick'>" + order.fullOrder() + "</span></li>");
  
     $(".pick").last().click(function () {
         $("#show-order").show()
-        $("#show-order h4").text(newOrder.order());;
+        $("#show-order h4").text(newOrder.fullOrder());;
         $(".checkbox").text(order.size);
         $(".crust").text(order.crust);
         $(".flavour").text(order.flavour);
